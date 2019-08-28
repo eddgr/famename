29edd1d98b9fd418e3c9b499bafa2471bc1ac56a
Edgar Ong <emailedgar@gmail.com>
2019-08-28 11:16:41 -0400

    update(RANK): show datatable only on rank page

--- usage.py
+++ usage.py
@@ -15,13 +15,13 @@ import dash_table
 df = pd.read_csv('http://data.cityofnewyork.us/api/views/25th-nujf/rows.csv')
 # df = pd.read_csv('./data/Popular_Baby_Names.csv')
 
+# secondary dataframe
 dff = df[df['Rank'] < 6]
 dff['Child\'s First Name'] = dff['Child\'s First Name'].str.upper()
 dff['index'] = range(1, len(dff) + 1)
 # incorporate gender or ethnicity filters (?)
 rank5_names = dff.groupby('Child\'s First Name')['Count'].sum()
 sorted_rank5 = rank5_names.sort_values(ascending=False)
-
 dfff = pd.DataFrame({'Name': sorted_rank5.keys().to_list(), 'Count': sorted_rank5.to_list()})
 # dff[dff['Gender'] == gender] + .groupby
 # dff[dff['Ethnicity'] == ethnicity] + .groupby
@@ -32,12 +32,11 @@ app = dash.Dash(__name__)
 app.title = 'Fame Name'
 
 # LAYOUT
+# dropdown name rendering
 options = []
 first_name = df['Child\'s First Name']
 for name in first_name:
     options.append({'label': name.capitalize(), 'value': name.upper()})
-
-PAGE_SIZE = 5
 
 app.layout = html.Div([
     famename.Famename(
@@ -49,29 +48,31 @@ app.layout = html.Div([
     ),
     dcc.Dropdown(id='compare_dropdown', options=options, placeholder='Select names to compare...', multi=True, style={'display': 'none'}),
     dcc.Graph(id='output_graph', style={'display': 'none'}),
-    # dash_table.DataTable(
-    #     id='rank_table',
-    #     columns=[
-    #         {'name': 'Name', 'id': 'Name'},
-    #         {'name': 'Count', 'id': 'Count'}
-    #     ],
-    #     data=pd.DataFrame({'Name': sorted_rank5.keys().to_list(), 'Count': sorted_rank5.to_list()}).to_dict('records')
-    # )
-    dash_table.DataTable(
-        id='rank_table',
-        columns=[
-            {"name": i, "id": i} for i in sorted(dfff.columns)
+    html.Div(
+        id='rank_table_container',
+        children=[
+            dash_table.DataTable(
+                id='rank_table',
+                columns=[
+                    {"name": 'Name', "id": 'Name'},
+                    {"name": 'Count', "id": 'Count'},
+                    # {"name": i, "id": i} for i in sorted(dfff.columns)
+                ],
+                page_current=0,
+                page_size=5,
+                page_action='custom',
+                sort_action='custom',
+                sort_mode='single',
+                sort_by=[]
+            )
         ],
-        page_current=0,
-        page_size=PAGE_SIZE,
-        page_action='custom',
-        sort_action='custom',
-        sort_mode='single',
-        sort_by=[]
+        style={'display': 'none'}
     )
+
 ])
 
 # CALLBACKS
+# datatable
 @app.callback(
     Output('rank_table', 'data'),
     [
@@ -94,6 +95,7 @@ def update_table(page_current,page_size,sort_by):
     return dffff.iloc[
         page_current*page_size:(page_current+ 1)*page_size
     ].to_dict('records')
+
 # resets graph on current page
 @app.callback(
     Output('compare_dropdown', 'value'),
@@ -102,13 +104,23 @@ def update_table(page_current,page_size,sort_by):
 def set_page(page_name):
     return []
 
-# hide dropdown
+# hide compare_dropdown
 @app.callback(
     Output('compare_dropdown', 'style'),
     [Input('react', 'currentPage')]
 )
 def hide_dropdown(page_name):
     if page_name == 'GenderSelectContainer' or page_name == 'Rank':
+        return {'display': 'none'}
+    return {'display': 'block'}
+
+# hide rank_table_container
+@app.callback(
+    Output('rank_table_container', 'style'),
+    [Input('react', 'currentPage')]
+)
+def hide_dropdown(page_name):
+    if page_name == 'GenderSelectContainer' or page_name == 'Compare':
         return {'display': 'none'}
     return {'display': 'block'}
 
